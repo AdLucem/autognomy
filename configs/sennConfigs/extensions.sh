0866423033f48fcd940bc3c0bb3c4c15dcc9e6ae
AdLucem <ghosalatreyee@gmail.com>
2021-05-30 01:26:36 +0530

    it works end-to-end!

--- configs/sennConfigs/extensions.sh
+++ configs/sennConfigs/extensions.sh
@@ -4,22 +4,19 @@ echo "Installing extensions..."
 
 # caffeine
 cd ~
-git clone git://github.com/eonpatapon/gnome-shell-extension-caffeine.git
+git clone http://github.com/eonpatapon/gnome-shell-extension-caffeine.git
 cd gnome-shell-extension-caffeine
 ./update-locale.sh
 glib-compile-schemas --strict --targetdir=caffeine@patapon.info/schemas/ caffeine@patapon.info/schemas
 cp -r caffeine@patapon.info ~/.local/share/gnome-shell/extensions/
 cd ~/.local/share/gnome-shell/extensions/caffeine@patapon.info
-make
-make install
 
 # coverflow alt-tab
 cd ~
 git clone https://github.com/dmo60/CoverflowAltTab.git
 cd CoverflowAltTab
-cp CoverflowAltTab@dmo60.de ~/.local/share/gnome-shell/extensions/
+cp -r CoverflowAltTab@dmo60.de ~/.local/share/gnome-shell/extensions/
 cd ~/.local/share/gnome-shell/extensions/CoverflowAltTab@dmo60.de
-make all
 
 # dash to dock
 cd ~
@@ -34,7 +31,6 @@ sudo apt install lm-sensors
 git clone https://github.com/UshakovVasilii/gnome-shell-extension-freon.git
 cd gnome-shell-extension-freon
 glib-compile-schemas freon@UshakovVasilii_Github.yahoo.com/schemas/
-mkdir -p ~/.local/share/gnome-shell/extensions
-cp -r freon@UshakovVasilii_Github.yahoo.com ~/.local/share/gnome-shell/extensions/.
+cp -r freon@UshakovVasilii_Github.yahoo.com ~/.local/share/gnome-shell/extensions/
 sudo sensors-detect
 
