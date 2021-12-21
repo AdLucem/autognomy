80a414cdf2f5910668f91b4b05fb6f819222e994
AdLucem <ghosalatreyee@gmail.com>
2021-12-21 12:42:16 +0530

    storing credentials on this PC

--- git-workflows/personal.sh
+++ git-workflows/personal.sh
@@ -3,6 +3,4 @@
 # set up git credentials on personal PC
 git config --global credential.helper store
 
-git add .
-git commit -m "random commit to store credentials"
-git push -u origin master
+git pull
