cb5dff9c458e01f03ff7d69263e00ada17b90b2b
AdLucem <ghosalatreyee@gmail.com>
2021-12-21 12:41:26 +0530

    random commit to store credentials

---
+++ git-workflows/personal.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# set up git credentials on personal PC
+git config --global credential.helper store
+
+git add .
+git commit -m "random commit to store credentials"
+git push -u origin main
