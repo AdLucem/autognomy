738b4b84668ecc8ebbba0424265b28f3c1ad70a4
AdLucem <ghosalatreyee@gmail.com>
2021-12-29 14:01:56 +0530

    stored some emacs configs; haven't added them to Aesthetic yet

---
+++ configs/sennConfigs/install_prelude.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -L https://github.com/bbatsov/prelude/raw/master/utils/installer.sh | sh
+
--- git-workflows/personal.sh
+++ git-workflows/personal.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# set up git identity on personal PC
+git config --global user.email "ghosalatreyee@gmail.com"
+git config --global user.name "AdLucem"
+
 # set up git credentials on personal PC
 git config --global credential.helper store
 
