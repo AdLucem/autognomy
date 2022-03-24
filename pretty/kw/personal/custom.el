e11bdbf1badf5c5d1b8dc6d54f3bf42e15ca2edf
AdLucem <ghosalatreyee@gmail.com>
2022-03-24 19:54:53 +0530

    updated haskell version

---
+++ pretty/kw/personal/custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("a6fc75241bcc7ce6f68dcfd0de2d4c4bd804d0f8cd3a9f08c3a07654160e9abe" "cfd51857f5e80eddece7eb5d30b9afce81f442707207e0d636250d03978a66ec" "3d4df186126c347e002c8366d32016948068d2e9198c496093a96775cc3b3eaa" "db7f422324a763cfdea47abf0f931461d1493f2ecf8b42be87bbbbbabf287bfe" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" default)))
+ '(package-selected-packages
+   (quote
+    (lean-mode flatui-dark-theme noctilux-theme oceanic-theme abyss-theme underwater-theme solarized-theme yaml-mode web-mode lsp-ui lsp-mode json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company counsel swiper ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode git-modes git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
---
+++ pretty/kwInTheSun/personal/preload/preload.el
@@ -0,0 +1,8 @@
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(add-to-list 'custom-theme-load-path "~/.emacs.d")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/elpa")
+
+(setq prelude-theme 'adwaita)
+(set-face-attribute 'default nil :height 190)
+
+(set-language-environment "UTF-8")
