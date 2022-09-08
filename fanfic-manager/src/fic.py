6cbbcdda7ae9951962dc40d72caaf260c679b0ed
AdLucem <ghosalatreyee@gmail.com>
2022-09-08 23:11:55 +0530

    finally the fanfic sorting is working

---
+++ fanfic-manager/src/fic.py
@@ -0,0 +1,84 @@
+import AO3
+import signal
+
+
+def retrieve_with_sigalarm(work_id, max_time):
+    """Retrieve a fic, but kill function if it takes time > max_time seconds"""
+
+    def timeout_handler(num, stack):
+        print("Received SIGALRM: fic retrieval taking too long")
+        raise Exception("Fic retrieval taking too long")
+
+    signal.signal(signal.SIGALRM, timeout_handler)
+
+    signal.alarm(max_time)
+
+    try:
+        work = AO3.Work(work_id, load_chapters=False)
+    except Exception as ex:
+        print(ex)
+        work = None
+
+    return work
+
+
+class Fic:
+    """My own class for storing a Fic object"""
+
+    def __init__(self, work_id="", from_dict=False, d={}):
+
+        if from_dict:
+            self.from_dict(d)
+            self.url = "https://archiveofourown.org/works/" + str(self.work_id)
+
+        elif not from_dict:
+            self.work_id = work_id 
+            self.url = "https://archiveofourown.org/works/" + str(self.work_id)
+
+            self.work = retrieve_with_sigalarm(work_id, 10)
+
+            if self.work:
+                self.title = self.work.title
+                self.fandoms = self.work.fandoms
+
+                self.tags = self.work.tags
+                self.characters = self.work.characters
+                self.relationships = self.work.relationships
+                self.summary = self.work.summary
+
+    def __repr__(self):
+
+        s = ""
+        s += "=============================================\n"
+        s += '[{TITLE}]({URL})\n'.format(TITLE=self.title, URL=self.url)
+        s += str(self.fandoms) + "\n"
+        s += "---------------------------------------------\n"
+        s += "Characters: " + str(self.characters) + "\n"
+        s += "Tags: " + str(self.tags) + "\n"
+        s += "Relationships: " + str(self.relationships) + "\n"
+        s += "---------------------------------------------\n"
+        s += self.summary + "\n"
+        s += "---------------------------------------------\n"
+        return s
+
+    def to_dict(self):
+
+        d = {"work_id": self.work_id,
+             "title": self.title,
+             "fandoms": self.fandoms,
+             "tags": self.tags,
+             "characters": self.characters,
+             "relationships": self.relationships,
+             "summary": self.summary}
+
+        return d
+        
+    def from_dict(self, d):
+
+        self.work_id = d["work_id"]
+        self.title = d["title"]
+        self.fandoms = d["fandoms"]
+        self.tags = d["tags"]
+        self.characters = d["characters"]
+        self.relationships = d["relationships"]
+        self.summary = d["summary"]
---
+++ fanfic-manager/src/main.py
@@ -0,0 +1,109 @@
+import AO3
+import argparse
+import json
+from fic import Fic
+
+
+def login(username, password):
+    """Start a session logged in as an user with given config. Return: a ao3-api `session` object"""
+
+    session = AO3.Session(username, password)
+    return session
+
+
+def get_m4l(session):
+    """Get all works in Marked For Later. Returns: a list of [Work]"""
+
+    works_for_later = session.get_marked_for_later()
+    return works_for_later
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Download all fics in your Marked For Later list, sort by fandom and store in a easily readable markdown file')
+    parser.add_argument('--username', type=str, help="Username of account.")
+    parser.add_argument('--password', type=str, help="Password of account.")
+
+    args = parser.parse_args()
+
+    with open("m4l.json") as f:
+        fics_json = json.load(f)
+
+    fics = []
+    for fic_json in fics_json:
+        obj = Fic(from_dict=True, d=fic_json)
+        fics.append(obj)
+
+    fandoms = {}
+    for fic in fics:
+        fandom = fic.fandoms[0]
+        print(fandom)
+
+        if fandom in fandoms:
+            fandoms[fandom].append(fic)
+        else:
+            fandoms[fandom] = [fic]
+
+    for fandom in fandoms:
+        print("+++++++++++++++++++++++++++++++++++")
+        print(fandom)
+        for fic in fandoms[fandom]:
+            print(fic)
+        print("+++++++++++++++++++++++++++++++++++")
+
+    """
+    session = login(args.username, args.password)
+    
+    works4l = get_m4l(session)
+    # with open("m4l.txt") as f:
+    #    data = [s.rstrip("\n") for s in f.readlines()]
+
+    works = []
+    
+    for work in works4l:
+        work_id = work.id
+        print("Retrieving work {work_id}".format(work_id=work_id))
+        work_obj = Fic(work_id=work_id)
+        if work_obj.work:
+            work_dict = work_obj.to_dict()
+            works.append(work_dict)
+
+    with open("m4l.json", "w+") as f:
+        json.dump(works, f)
+    """
+
+"""
+    storage = []
+
+    worksToLoad = []
+    for work_id in works4l:
+        try: 
+            worksToLoad.append(AO3.Work(work_id, load=False))
+        except AttributeError:
+            continue
+"""
+"""
+    for work_id in works4l:
+        try:
+            work_obj = Fic(work_id=work_id)
+            storage.append(work_obj.to_dict())
+        except AttributeError:
+            continue
+
+    with open("works_for_later.json", "w+") as f:
+        json.dump(storage, f)
+
+
+    session = login(args.username, args.password)
+    works4l = get_m4l(session)
+
+    for work in works4l:
+        print(work.id)
+
+    for work in works4l:
+        try:
+            work_obj = Fic(work_id=work.id)
+            print(work_obj)
+        except AttributeError:
+            continue
+"""
